4ccb9629d93efc39bea2e1dcc0abc00153362cb4
yael <howitzer117@outlook.com>
2024-07-27 13:48:22 -0600

    feat: suerte

--- API/src/main/java/com/MimitosShop/API/ApiApplication.java
+++ API/src/main/java/com/MimitosShop/API/ApiApplication.java
@@ -6,6 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiApplication {
 
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
+
+	@Value("${spring.datasource.username}")
+	private String datasourceUsername;
+
+	@Value("${spring.datasource.password}")
+	private String datasourcePassword;
+
+	public void run(String... args) throws Exception {
+		System.out.println("Datasource URL: " + datasourceUrl);
+		System.out.println("Datasource Username: " + datasourceUsername);
+		System.out.println("Datasource Password: " + datasourcePassword);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
