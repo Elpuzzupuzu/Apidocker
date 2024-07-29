d8b279f28d2f68a93776ba18f891510df0e8cb9e
yael <howitzer117@outlook.com>
2024-07-29 08:57:21 -0600

    endpoints cambiados y cors

--- API/src/main/java/com/MimitosShop/API/Controller/CorsConfig.java
+++ API/src/main/java/com/MimitosShop/API/Controller/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         // Permitir todos los orígenes necesarios para desarrollo y despliegue
         config.addAllowedOrigin("http://localhost");
-        config.addAllowedOrigin("http://127.0.0.1:5500"); // Agregar el origen de tu frontend
+        config.addAllowedOrigin("*"); // Agregar el origen de tu frontend
         // config.addAllowedOrigin("*"); // Permitir todos los orígenes (opcional, para pruebas). Úsalo con precaución.
 
         // Permitir todos los headers y métodos que necesites
